ad52a8cfffac11e3ff30f4fde52518979c7b3be9
Morten Berg <mortenberg80@gmail.com>
2013-05-23 17:33:23 +0200

    Added mongodb client which prints to sysout

--- src/main/scala/no/iterate/scozon/web/IrcLog.scala
+++ src/main/scala/no/iterate/scozon/web/IrcLog.scala
@@ -3,12 +3,27 @@ package no.iterate.scozon.web
 import unfiltered.filter.Plan
 import unfiltered.request._
 import unfiltered.response._
+import com.mongodb.casbah.Imports._
+import com.mongodb.casbah.commons.conversions.scala.RegisterJodaTimeConversionHelpers
+import org.joda.time.DateTime
+import com.mongodb.casbah.commons.conversions.scala.DeregisterJodaTimeConversionHelpers
 
 object IrcLog extends Plan {
+  
+  val mongoDb = MongoClient("192.168.1.60")("scozon")
 
   def intent = {
     case Path("/") =>
-        Html(<h1>Hello world!</h1>)
+      
+      val q = MongoDBObject("command" -> "PRIVMSG")
+      val cursor = mongoDb("log").find(q).sort(MongoDBObject("time" -> -1)).limit(5)
+      cursor.toList.reverse.foreach { x =>
+      	x.getAs[String]("trailing") match {
+      		case Some(text) => println(text) 
+      		case None => // do nothing
+      	}
+      }
+      Html(<h1>Hello world!</h1>) 
   }
   
 }
