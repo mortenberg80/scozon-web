dc37b548e30b0700eaa664e4c09ee10cb814150a
Fredrik Vraalsen <fredrik@vraalsen.no>
2013-05-23 17:43:39 +0200

    Show Irc log results on webpage

--- src/main/scala/no/iterate/scozon/web/IrcLog.scala
+++ src/main/scala/no/iterate/scozon/web/IrcLog.scala
@@ -17,13 +17,10 @@ object IrcLog extends Plan {
       
       val q = MongoDBObject("command" -> "PRIVMSG")
       val cursor = mongoDb("log").find(q).sort(MongoDBObject("time" -> -1)).limit(5)
-      cursor.toList.reverse.foreach { x =>
-      	x.getAs[String]("trailing") match {
-      		case Some(text) => println(text) 
-      		case None => // do nothing
-      	}
-      }
-      Html(<h1>Hello world!</h1>) 
+      
+      val trailings = cursor.flatMap { x => x.getAs[String]("trailing") }.toList.reverse
+      
+      Html(<ul>{ trailings map { t => <li>{t}</li>}}</ul>) 
   }
   
 }
